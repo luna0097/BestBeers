ac939ed3067b78926aeafcdac68b1a620deb054b
luna0097 <luna.apolinar97@gmail.com>
2022-06-22 23:53:18 -0600

    depuracion: se renombraron modulos, se elimino modulo dashboard, se creo la estuctura base de carpetas, se agrego nuevo recurso drawable

--- app/src/main/java/com/example/bestbeers/ui/favorite/FavoritesViewModel.kt
+++ app/src/main/java/com/example/bestbeers/ui/favorite/FavoritesViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.bestbeers.ui.notifications
+package com.example.bestbeers.ui.favorite
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class FavoritesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
